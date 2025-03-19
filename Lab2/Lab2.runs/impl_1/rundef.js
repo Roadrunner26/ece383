eadaa713c3e5065b652eb90ba7d57234c0d53428
Zimmerman <117304353+Roadrunner26@users.noreply.github.co>
2025-03-18 22:42:47 -0600

    Reset path

--- Lab2/Lab2.runs/impl_1/rundef.js
+++ Lab2/Lab2.runs/impl_1/rundef.js
@@ -24,7 +24,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log lab2.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source lab2.tcl -notrace" );
 
--- Lab2/Lab2.srcs/sources_1/imports/lab2_code_for_cadets/lab2_fsm.vhdl
+++ Lab2/Lab2.srcs/sources_1/imports/lab2_code_for_cadets/lab2_fsm.vhdl
@@ -1,14 +1,11 @@
 ----------------------------------------------------------------------------------
--- Name:	Grantham Zimmerman
--- Date:	Spring 2025
+-- Name:	Template by George your (modified from Jeff Falkinburg)
+-- Date:	Spring 2023
 -- File:    lab2_fsm.vhd
 -- HW:	    Lab 2 
 -- Pupr:	Lab 2 Finite State Machine for the write circuitry.  
 --
--- Doc:	Sam Quick and I unsuccessfully verbally talked about trigger logic, reset logic, and worked on the datapath to see 
---if we could successfully get the monitor to show a scroll. This did not work. Lt Col Trimble showed me through a youtube video 
---how to debug my datapath to get simlive to show up. He also showed me how to deal with my fromADCX signals with a clock process 
---to ensure triggering showed up correctly.
+-- Doc:	Adapted from Dr Coulston's Lab exercise
 -- 	
 -- Academic Integrity Statement: I certify that, while others may have 
 -- assisted me in brain storming, debugging and validating this program, 
